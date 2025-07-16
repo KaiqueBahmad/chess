7331bf011145875a92e45a6fa9bebf6973a5c2bf
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-07-16 03:10:11 -0300

    ajuste run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 mkdir -p bin
 tcc -o bin/chess src/chess.c src/main.c
 tcc -shared -fPIC -o bin/libchess.so src/chess.c
-gzexe bin/chess
+# reduz o tamanho mas só vai funcionar se tiver rodando num os com /bin/sh
+#gzexe bin/chess
+#gzexe bin/libchess.so
 rm -f bin/*~
 ./bin/chess
